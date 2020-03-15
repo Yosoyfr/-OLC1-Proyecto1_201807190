0f64cc1276c6ef183e43df204c6a4646fe36c0db
Francisco Suarez <39706929+Yosoyfr@users.noreply.github.com>
2020-03-14 20:17:39 -0600

    Operadores

---
+++ [OLC1]Proyecto1_201807190/Operador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OLC1_Proyecto1_201807190
+{
+    interface Operador
+    {
+
+    }
+}
